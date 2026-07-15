347c55a392602670fe5192e1285a2ec743943a1a
Nick Fraser <icanlosh@gmail.com>
2026-07-15 20:04:12 +0100

    [recipe] Updated recipe to use a downgraded GLIBC version

--- recipes/screen/build.sh
+++ recipes/screen/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+perl -0pi -e "s@int   AddXChars \(char \*, int, char \*\);\n@int   AddXChars (char *, int, char *);\nstatic inline int xatoi (const char *str)\n{\n\tint sign, value;\n\n\twhile (*str == ' ' || ((unsigned char)*str >= 9 && (unsigned char)*str <= 13))\n\t\tstr++;\n\tsign = 1;\n\tif (*str == '-' || *str == '+')\n\t\tsign = *str++ == '-' ? -1 : 1;\n\tvalue = 0;\n\twhile (*str >= '0' && *str <= '9') {\n\t\tvalue = value * 10 + (*str - '0');\n\t\tstr++;\n\t}\n\treturn sign < 0 ? -value : value;\n}\n@" misc.h
+
+perl -0pi -e 's/\batoi\(/xatoi(/g' ansi.c attacher.c fileio.c process.c screen.c socket.c termcap.c tty.c
+
 ./configure \
   --prefix="${PREFIX}" \
   --disable-pam \
