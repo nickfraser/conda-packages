ed8f37bf6ae1dfea928e2cfdf11b0a2e7ff116eb
Nick Fraser <icanlosh@gmail.com>
2026-09-12 10:00:03 +0000

    [recipe] Updated screen to `v5.0.2`

--- recipes/screen/build.sh
+++ recipes/screen/build.sh
@@ -14,6 +14,6 @@ perl -0pi -e 's/\batoi\(/xatoi(/g' ansi.c attacher.c fileio.c process.c screen.c
 make -j"${CPU_COUNT}"
 
 install -d "${PREFIX}/bin" "${PREFIX}/share/screen/utf8encodings"
-install -m 755 "screen" "${PREFIX}/bin/screen-5.0.1"
-ln -s "screen-5.0.1" "${PREFIX}/bin/screen"
+install -m 755 "screen" "${PREFIX}/bin/screen-5.0.2"
+ln -s "screen-5.0.2" "${PREFIX}/bin/screen"
 install -m 644 utf8encodings/?? "${PREFIX}/share/screen/utf8encodings/"
